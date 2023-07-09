43ed38786b1846446571722b58ecb039161eceae
subha <subhasishduttshuvo2018@gmail.com>
2023-07-10 02:19:51 +0600

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -21,11 +21,13 @@ class _WeatherAppState extends State<WeatherApp> {
   bool isLoading = false;
   bool isError = false;
   WeatherData? weatherData;
+  TextEditingController _locationController = TextEditingController();
+  bool isSearchBoxVisible = false;
 
   @override
   void initState() {
     super.initState();
-    fetchWeatherData('Bangladesh'); // Default location
+    fetchWeatherData('Dhaka'); // Default location
   }
 
   Future<void> fetchWeatherData(String location) async {
@@ -57,6 +59,12 @@ class _WeatherAppState extends State<WeatherApp> {
     }
   }
 
+  void toggleSearchBox() {
+    setState(() {
+      isSearchBoxVisible = !isSearchBoxVisible;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -73,56 +81,154 @@ class _WeatherAppState extends State<WeatherApp> {
             : isError
             ? Center(child: Text('Error fetching data'))
             : Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                weatherData != null ? weatherData!.getLocation() : '',
-                style: TextStyle(
-                  fontSize: 28,
-                  color: Colors.white,
+          child: SingleChildScrollView(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  weatherData != null ? weatherData!.getLocation() : '',
+                  style: TextStyle(
+                    fontSize: 28,
+                    color: Colors.white,
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                weatherData != null ? weatherData!.getTemperature() : '',
-                style: TextStyle(
-                  fontSize: 48,
-                  color: Colors.white,
-                  fontFamily: 'CustomFont',
+                SizedBox(height: 10),
+                Text(
+                  weatherData != null ? weatherData!.getTemperature() : '',
+                  style: TextStyle(
+                    fontSize: 48,
+                    color: Colors.white,
+                    fontFamily: 'CustomFont',
+                  ),
                 ),
-              ),
-              SizedBox(height: 10),
-              Text(
-                weatherData != null ? weatherData!.getDescription() : '',
-                style: TextStyle(
-                  fontSize: 24,
-                  color: Colors.white,
-                  fontFamily: 'CustomFont',
+                SizedBox(height: 10),
+                Text(
+                  weatherData != null ? weatherData!.getDescription() : '',
+                  style: TextStyle(
+                    fontSize: 24,
+                    color: Colors.white,
+                    fontFamily: 'CustomFont',
+                  ),
                 ),
-              ),
-              SizedBox(height: 20),
-              CachedNetworkImage(
-                imageUrl:
-                weatherData != null ? weatherData!.getIconUrl() : '',
-                placeholder: (context, url) =>
-                    CircularProgressIndicator(),
-                errorWidget: (context, url, error) => Icon(Icons.error),
-                height: 200, // Increase the height
-                width: 200, // Increase the width
-              ),
-              SizedBox(height: 10),
-              Text(
-                weatherData != null
-                    ? 'Last Updated: ${weatherData!.getLastUpdated()}'
-                    : '',
-                style: TextStyle(
-                  fontSize: 16,
-                  color: Colors.white,
+                SizedBox(height: 20),
+                CachedNetworkImage(
+                  imageUrl:
+                  weatherData != null ? weatherData!.getIconUrl() : '',
+                  placeholder: (context, url) =>
+                      CircularProgressIndicator(),
+                  errorWidget: (context, url, error) =>
+                      Icon(Icons.error),
+                  fit: BoxFit.contain, // Resize the image to fit
+                  height: 150,
                 ),
-              ),
-            ],
+                SizedBox(height: 10),
+                Column(
+                  children: [
+                    Text(
+                      weatherData != null ? 'Max Temperature' : '',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      weatherData != null
+                          ? '${weatherData!.getMaxTemperature()}'
+                          : '',
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      weatherData != null ? 'Min Temperature' : '',
+                      style: TextStyle(
+                        fontSize: 25,
+                        color: Colors.white,
+                      ),
+                    ),
+                    Text(
+                      weatherData != null
+                          ? '${weatherData!.getMinTemperature()}'
+                          : '',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.white,
+                      ),
+                    ),
+                    SizedBox(height: 30),
+                    Text(
+                      weatherData != null
+                          ? 'Last Updated: ${weatherData!.getLastUpdated()}'
+                          : '',
+                      style: TextStyle(
+                        fontSize: 19,
+                        color: Colors.white,
+                      ),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 30),
+                Visibility(
+                  visible: isSearchBoxVisible,
+                  child: Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 30),
+                    child: TextField(
+                      controller: _locationController,
+                      style: TextStyle(color: Colors.white),
+                      decoration: InputDecoration(
+                        hintText: 'Enter city/country',
+                        hintStyle: TextStyle(color: Colors.white70),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: toggleSearchBox,
+                  child: Text(
+                    isSearchBoxVisible ? 'Cancel' : 'Change Location',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                  ),
+                ),
+                SizedBox(height: 10),
+                ElevatedButton(
+                  onPressed: () {
+                    final String location =
+                    _locationController.text.trim();
+                    if (location.isNotEmpty) {
+                      fetchWeatherData(location);
+                    }
+                  },
+                  child: Text(
+                    'Get Weather',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  style: ElevatedButton.styleFrom(
+                    primary: Colors.blue,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+        floatingActionButton: Visibility(
+          visible: !isSearchBoxVisible,
+          child: FloatingActionButton(
+            onPressed: toggleSearchBox,
+            child: Icon(Icons.search),
+            backgroundColor: Colors.blue,
           ),
         ),
       ),
@@ -133,6 +239,8 @@ class _WeatherAppState extends State<WeatherApp> {
 class WeatherData {
   final String location;
   final int temperature;
+  final int maxTemperature;
+  final int minTemperature;
   final String description;
   final String icon;
   final String lastUpdated;
@@ -140,6 +248,8 @@ class WeatherData {
   WeatherData({
     required this.location,
     required this.temperature,
+    required this.maxTemperature,
+    required this.minTemperature,
     required this.description,
     required this.icon,
     required this.lastUpdated,
@@ -154,9 +264,12 @@ class WeatherData {
     return WeatherData(
       location: name,
       temperature: main['temp'].toInt(),
+      maxTemperature: main['temp_max'].toInt(),
+      minTemperature: main['temp_min'].toInt(),
       description: weather['description'],
       icon: weather['icon'],
-      lastUpdated: DateTime.fromMillisecondsSinceEpoch(dt * 1000).toString(),
+      lastUpdated:
+      DateTime.fromMillisecondsSinceEpoch(dt * 1000).toString(),
     );
   }
 
@@ -166,6 +279,14 @@ class WeatherData {
 
   String getTemperature() {
     return '$temperature°C';
+  }
+
+  String getMaxTemperature() {
+    return '${maxTemperature}°C';
+  }
+
+  String getMinTemperature() {
+    return '${minTemperature}°C';
   }
 
   String getDescription() {
@@ -178,7 +299,8 @@ class WeatherData {
 
   String getLastUpdated() {
     final dateTime = DateTime.parse(lastUpdated);
-    final formattedDateTime = DateFormat('MMM d, yyyy hh:mm:ss a').format(dateTime);
+    final formattedDateTime =
+    DateFormat('MMM d, yyyy hh:mm:ss a').format(dateTime);
     return formattedDateTime;
   }
 }
